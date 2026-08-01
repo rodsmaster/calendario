ef55cb9694d82856828ce4d963f0ad2cbaf95216
Alex Santos <alex_rds@outlook.com>
2026-08-01 20:09:10 -0300

    Update calendar-config.js

--- scripts/calendar-config.js
+++ scripts/calendar-config.js
@@ -1,22 +1,14 @@
 export const calendarsConfig = [
-
+    {
+        id: "feriados_br",
+        nome: "Feriados Brasil",
+        cor: "#10b981",
+        url: "https://p46-calendars.icloud.com/holidays/br_pt.ics"
+    },
     {
         id: "fases_lua",
         nome: "Fases da Lua",
-        cor: "#71f916ff",
+        cor: "#f97316",
         url: "https://calendar.google.com/calendar/ical/ht3jlfaac5lfd6263ulfh4tql8%40group.calendar.google.com/public/basic.ics"
-    },
-    {
-        id: "calendario_google",
-        nome: "Google Agenda",
-        cor: "#df42f4ff",
-        url: "https://calendar.google.com/calendar/ical/agenda.genia2%40gmail.com/private-5388cd59b201e7db6a65880ec3212150/basic.ics"
-    },
-    {
-        id: "Calendario_outlook",
-        nome: "Outlook Agenda",
-        cor: "#0078D4",
-        url: "https://outlook.live.com/owa/calendar/00000000-0000-0000-0000-000000000000/be7aaee7-e466-4fd7-b461-f1632e553103/cid-7628ED0DD7187C6B/calendar.ics"
-    },
-
+    }
 ];
