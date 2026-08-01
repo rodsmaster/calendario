b1a19fac99c4784d5d8110725c62234dbb5e337d
Alex Santos <alex_rds@outlook.com>
2026-08-01 20:09:44 -0300

    Delete scripts/firebase-config_.js

--- scripts/firebase-config_.js
+++
@@ -1,9 +0,0 @@
-// Substitua pelas suas credenciais do Firebase Console
-export const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_PROJETO.firebaseapp.com",
-    projectId: "SEU_PROJETO",
-    storageBucket: "SEU_PROJETO.appspot.com",
-    messagingSenderId: "SEU_SENDER_ID",
-    appId: "SEU_APP_ID"
-};
